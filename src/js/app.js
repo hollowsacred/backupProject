fe52a25019cd9f840dff623864ba4dbb5c5f6277
Aleksandr <ligionsocial@gmail.com>
2022-04-25 00:52:37 +0500

    second commit

--- src/js/app.js
+++ src/js/app.js
@@ -1,26 +1,44 @@
 import * as Slick from "./modules/slick.min.js";
 import * as functions from "./modules/functions.js";
 console.log("Кабаок");
-$(document).ready(function(){
-    $('.carousel__inner').slick({
+$(document).ready(function () {
+  $(".carousel__inner").slick({
     //    adaptiveHeight:true,
-       autoplay:false,
-       prevArrow: `<button type="button" class="slick-prev"><img src="img/prevArrow.png"></button>`,
-       nextArrow: `<button type="button" class="slick-next"><img src="img/nextArrow.png"></button>`,
-      });
-     
-  });
-
-  let slider = tns({
-    container: '.carousel__tiny',
-    items: 1,
-    slideBy: 'page',
     autoplay: false,
-    controlsText:[
-        `<img src="img/prevArrow.png"></img>`,`<img src="img/nextArrow.png"></img>`
-    ],
-    nav:false,
+    prevArrow: `<button type="button" class="slick-prev"><img src="img/prevArrow.png"></button>`,
+    nextArrow: `<button type="button" class="slick-next"><img src="img/nextArrow.png"></button>`,
   });
+});
 
-   
-  
+let slider = tns({
+  container: ".carousel__tiny",
+  items: 1,
+  slideBy: "page",
+  autoplay: false,
+  controlsText: [
+    `<img src="img/prevArrow.png"></img>`,
+    `<img src="img/nextArrow.png"></img>`,
+  ],
+  nav: false,
+});
+
+let catalog = document.querySelector(".catalog-item__body");
+let catalogActive = document.querySelector(".catalog-item__body_active");
+let count = 1;
+let link = document.querySelectorAll(".catalog-item__link");
+for (let i = 0; i < link.length; i++) {
+  link[i].addEventListener("click", function (evt) {
+    evt.preventDefault();
+    catalog.style.display = "none";
+    catalogActive.style.display = "block";
+
+    if (count % 2 == 0) {
+      catalog.style.display = "flex";
+      catalogActive.style.display = "none";
+      console.log("ребутош " + count);
+    }
+
+    count++;
+    console.log(count);
+  });
+}
